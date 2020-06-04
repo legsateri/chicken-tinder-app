aa50c5689cb21f0783ab534d56497288ad1a8fc4
Allegra Pusateri <allegrapusateri@gmail.com>
2020-06-04 15:57:26 -0500

    configure access to server api

--- src/config.js
+++ src/config.js
@@ -5,5 +5,7 @@ export default {
     WEATHER_KEY: "ddea905bc3f94e0bb0122110201205",
     RECIPE_ENDPOINT: "https://api.edamam.com/search",
     RECIPE_KEY: "8a82fe579dbb93f151caf8d8ca515c08",
-    RECIPE_ID: "0bc5a132"
+    RECIPE_ID: "0bc5a132",
+    API_ENDPOINT: "https://lit-lowlands-58882.herokuapp.com/api",
+    TOKEN_KEY: "chicken-tender-client-auth-token"
 }
