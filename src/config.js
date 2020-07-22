81360c0d181a5d39896b3afd47efe27bc63957a6
Allegra Pusateri <allegrapusateri@gmail.com>
2020-07-21 21:43:10 -0500

    updated to remote server rather than local server

--- src/config.js
+++ src/config.js
@@ -6,6 +6,6 @@ export default {
     RECIPE_ENDPOINT: "https://api.edamam.com/search",
     RECIPE_KEY: "8a82fe579dbb93f151caf8d8ca515c08",
     RECIPE_ID: "0bc5a132",
-    API_ENDPOINT: "http://localhost:8000/api",
+    API_ENDPOINT: "https://pure-sierra-61248.herokuapp.com/api",
     TOKEN_KEY: "chicken-tender-client-auth-token"
 };
